c652349e53e3d36077a7612b61d55d70cbfa127e
Akarsh Shukla <akarshshukla19@yahoo.com>
2021-03-15 21:00:27 +0000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {name: 'Akarsh Shukla', streetAddress: '6322 field flower trail'}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const nextEmployee = {...obj};
+    
+    nextEmployee[key] = value;
+    return nextEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const employee3 = {...obj};
+    delete employee3[key];
+    return employee3;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}// Write your solution in this file!
